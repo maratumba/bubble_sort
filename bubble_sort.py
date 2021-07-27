fc1b6cab1a7a1203ad2cd75eaf3a45f674d56038
gulyamani <dandi@gmail.com>
2021-07-27 13:29:00 +0200

    add lxml, start parsing

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,9 +1,10 @@
 import requests
 from requests.compat import urljoin
+from lxml import html
 
 API_URL = "https://api.stackexchange.com"
 base_params = {'site':'stackoverflow'}
-def get_accepted_answers(params):
+def get_accepted_answers():
     # this is not random yet
     params = {**base_params, 'order':'desc', 'sort':'votes', 'tagged':'python', 'intitle': 'bubble sort'}
     search_url = urljoin(API_URL, 'search')
@@ -15,10 +16,17 @@ def get_accepted_answers(params):
         return False
     
     # filter for questions with accepted answers
-    accepted_answer_ids =  [q['accepted_answer_id'] for q in questions if q['is_answered'] == True and 'accepted_answer_id' in q]
+    accepted_answer_ids = [q['accepted_answer_id'] for q in questions if q['is_answered'] == True and 'accepted_answer_id' in q]
+    return accepted_answer_ids
 
+def get_answer_url(answer_id):
+    return urljoin("https://stackoverflow.com/a/",str(answer_id))
 
-def get_answers_for_question(question_id)
-    answers_url = '/'.join(['questions', str(question_id), 'answers'])
-    url = urljoin(API_URL, answers_url)
-    answers = requests.get(url, params={**base_params, "sort":"votes"}).json()['items']
+def get_answer_html(answer_id):
+    answer_url = get_answer_url(answer_id)
+    resp = requests.get(answer_url)
+    if resp.status_code == 200:
+        return resp.content
+
+def parse_answer_html(answer_html):
+    # returns code only string
